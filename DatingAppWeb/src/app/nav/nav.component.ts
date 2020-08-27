5185581f411f33c191adb8178fb5df9824887740
oussama <oussama.siharde@live.fr>
2020-08-27 15:13:19 +0100

    extend user mo models and seeding data

--- DatingAppWeb/src/app/nav/nav.component.ts
+++ DatingAppWeb/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../_services/auth.service";
 import {AlertifyService} from "../_services/alertify.service";
 import {BsDropdownConfig} from "ngx-bootstrap/dropdown";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,9 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   constructor(private authService: AuthService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +27,8 @@ export class NavComponent implements OnInit {
         this.alertify.success("logged in successfully");
       },error => {
         this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['members'])
       }
     );
   }
@@ -35,6 +40,7 @@ export class NavComponent implements OnInit {
   logout(){
     this.alertify.message('Logged Out');
     localStorage.removeItem('token');
+    this.router.navigate(['home']);
   }
 
 
